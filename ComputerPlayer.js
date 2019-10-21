53131e5dee65626c2f92d89876e6a7a794b73d85
MoisesPotato <moiseslasaguas-github@yahoo.es>
2019-10-20 19:23:27 -0500

    Add files via upload

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -20,10 +20,10 @@ if (!displayingGraphics) {
     var playerTypes = main.playerTypes;
 }
 
-function synapse(toNeuron, w){
-    this.weight = w;
-    this.to = toNeuron;
-}
+//function synapse(toNeuron, w){
+//    this.weight = w;
+//    this.to = toNeuron;
+//}
 
 var neuron = function(ID){
     this.inputs = [];
@@ -56,7 +56,14 @@ layer.prototype.connect = function(aLayer, weights){
     }
 };
 
-var network = function(layerNos, weights){// three numbers
+var network = function(layerNos, weights){// layerNos is an array of 5 numbers: 
+    /*
+    0: Size of usual input layer
+    1: Size of Recursive input layer
+    2: Size of processed recursive input layer
+    3: Size of hidden layer
+    4: Output layer
+    */
     this.sizes = layerNos;
     this.layers = [];
     var ID = 0;
@@ -64,10 +71,13 @@ var network = function(layerNos, weights){// three numbers
         var newLayer = new layer(layerNos[l], ID);
         ID += layerNos[l];
         this.layers.push(newLayer);
-        if(l > 0){
-            this.layers[l].connect(this.layers[l-1], weights);
-        }
+        //connections
     }
+    this.layers[2].connect(this.layers[1], weights);
+    this.layers[2].connect(this.layers[2], weights);
+    this.layers[3].connect(this.layers[0], weights);
+    this.layers[3].connect(this.layers[2], weights);
+    this.layers[4].connect(this.layers[3], weights);
 };
 
 
@@ -126,18 +136,56 @@ network.prototype.neuronFind = function(ID){
 
 
 network.prototype.activate = function(inputArray){
-    if (inputArray.length < this.sizes[0]){
-        inputArray.push(new Array(this.sizes[0] - inputArray.length).fill(0));
-    } else if (inputArray.length > this.sizes[0]){
-        inputArray.splice(this.sizes[0], inputArray.length - this.sizes[0]);
+//    if (inputArray.length < this.sizes[0]){
+//        inputArray.push(new Array(this.sizes[0] - inputArray.length).fill(0));
+//    } else if (inputArray.length > this.sizes[0]){
+//        inputArray.splice(this.sizes[0], inputArray.length - this.sizes[0]);
+//    }
+    if (inputArray.length !== this.sizes[0] + 1){
+        return "Wrong size!!";
+    } else if (inputArray[this.sizes[0]][0] !== undefined && inputArray[this.sizes[0]][0].length !== this.sizes[1]){
+        console.log("Input size:");
+        console.log(inputArray[this.sizes[0]].length);
+        console.log("Expected size:");
+        console.log(this.sizes[1]);
+        return "Wrong size in the size!";
     }
     for (var chooseNeuron in this.layers[0].neuronList){
         var currentNeuron = this.layers[0].neuronList[chooseNeuron];
         currentNeuron.lit = inputArray[chooseNeuron];
     }
-    for (var chooseLayer = 1; chooseLayer < this.sizes.length; chooseLayer++){
+    let bulletList = inputArray[this.sizes[0]];
+    let nBullets = bulletList.length;
+    //// Feed input into recursive neurons
+    this.layers[2].neuronList.forEach(function(n){//Initialize everything to 0
+        n.lit = 0;    
+    });
+    
+    for(let bulletIndex in bulletList){
+        let b = bulletList[bulletIndex];
+        for (let n = 0; n < this.sizes[1]; n++){
+            this.layers[1].neuronList[n].lit = b[n]; 
+        }
+        let oldValues = {};
+        for (let j in this.layers[2].neuronList){//Copy lit values of network into old values
+            oldValues[this.layers[2].neuronList[j].ID] = this.layers[2].neuronList[j].lit;
+        }
+        for (let j in this.layers[2].neuronList){//Update the values
+            let n = this.layers[2].neuronList[j];
+            let sum = 0;
+            for (let otherNeuron in n.inputs){
+                    if (parseInt(otherNeuron) < this.sizes[0]){
+                        sum += n.inputs[otherNeuron] * this.neuronFind(otherNeuron).lit;
+                    } else {
+                        sum += n.inputs[otherNeuron] * oldValues[otherNeuron];
+                    }
+                }
+        }
+    }
+        
+    for (var chooseLayer = 3; chooseLayer < this.sizes.length; chooseLayer++){
         var currentLayer = this.layers[chooseLayer].neuronList;
-        for (var pickNeuron in currentLayer){
+        for (let pickNeuron in currentLayer){
             var thisNeuron = currentLayer[pickNeuron];
             var sum = 0;
             for (var otherNeuron in thisNeuron.inputs){
@@ -146,10 +194,12 @@ network.prototype.activate = function(inputArray){
             thisNeuron.lit = squash(sum);
         }
     }
-    var output = [];
-    for (var outNeuron in this.layers[this.sizes.length-1].neuronList){
-        output.push(this.layers[this.sizes.length-1].neuronList[outNeuron].lit);
-    }
+    let output = this.layers[this.sizes.length-1].neuronList.map(
+        (n) => n.lit
+    );
+//    for (var outNeuron in this.layers[this.sizes.length-1].neuronList){
+//        output.push(this.layers[this.sizes.length-1].neuronList[outNeuron].lit);
+//    }
     return output;
 };
 
@@ -177,6 +227,12 @@ function playGame(brain1, brain2, options){
         u.Player.Two.whoPlaying = brain1;
     }
     u.Player.Two.whoPlaying = brain2;
+    let angle = Math.random() * 0.01 - 0.005;
+    u.flipAround();
+    u.flipAround();
+    u.flipAround();
+    u.Player.One.vel = u.Player.One.vel.rot(angle);
+    u.Player.Two.vel = u.Player.Two.vel.rot(angle);
     if (options.random !== undefined && options.random){
         u.Player.One.pos.x = -200 + Math.random() * 400;
         u.Player.One.pos.y = -150 + Math.random() * 300;
@@ -191,7 +247,7 @@ function playGame(brain1, brain2, options){
     status.winner = "none";
     status.onePlayer = options.onePlayer;
     status.stepCount = 0;
-    while(status.playing){
+    while(status.playing && status.stepCount < 10000){
         gameStep(u,status);
         status.stepCount += 1;
     }
@@ -281,15 +337,16 @@ function playTournament(A, gameNumber, options){//A is an array of networks, num
             for (var awayPlayer = 0; awayPlayer < playerNumber; awayPlayer++){
                 if (homePlayer != awayPlayer){
                     for (var nthGame = 0; nthGame < gameNumber; nthGame++){
-                        var gameResult = playGame(A[homePlayer], A[awayPlayer]);
+                        var gameResult = playGame(A[homePlayer], A[awayPlayer],{onePlayer:false});
                         results[homePlayer][awayPlayer] = results[homePlayer][awayPlayer] + gameResult;
                         results[awayPlayer][homePlayer] = results[awayPlayer][homePlayer] - gameResult;
-                        gameResult = playGame(A[awayPlayer], A[homePlayer]);
+                        gameResult = playGame(A[awayPlayer], A[homePlayer],{onePlayer:false});
                         results[homePlayer][awayPlayer] = results[homePlayer][awayPlayer] - gameResult;
                         results[awayPlayer][homePlayer] = results[awayPlayer][homePlayer] + gameResult;
                     }
                 }
             }
+            fs.appendFileSync('log.txt', 'Games between '+(homePlayer)+' and the rest concluded.\n', writingCallback);
         }
         var score = Array(playerNumber).fill(0);
         for (homePlayer = 0; homePlayer < playerNumber; homePlayer++){
@@ -329,7 +386,7 @@ function improveSpecies(A, nGames, learningRate, options){
             thirdBest = score[contestant];
         }
     }
-    console.log("Winner is number "+winners+". Top score: "+topScore / nGames +". Second: "+secondBest / nGames +". Third: " + thirdBest / nGames);
+    console.log("Winner is number "+winners+". Top score: "+(topScore / nGames).toFixed(1) +". Second: "+secondBest / nGames +". Third: " + thirdBest / nGames);
     var winnersNumber = winners.length;
     var B = [];
     if (winnersNumber == playerNumber){
@@ -353,6 +410,81 @@ function improveSpecies(A, nGames, learningRate, options){
     return B;
 }
 
+function improveSpeciesGenetic(A, nGames, learningRate, options){
+    var playerNumber = A.length;
+    options.onePlayer = options.onePlayer || false;
+    var score;
+    if (options.onePlayer){
+        score = playTournamentOnePlayer(A, nGames, options);
+    } else {
+        score = playTournament(A, nGames, options);
+    }
+    var winners = Array(options.survivors).fill(-1);
+    var smallestWinningScore = 0;
+    var indices = [];
+    for (var contestant = 0; contestant < playerNumber; contestant++){
+        indices.push(contestant);
+    }
+    //console.log("Highest score: "+score[indices[0]]/ nGames);
+    //console.log("Lowest surviving score: "+score[indices[options.survivors - 1]]/ nGames);
+    console.log("Scores:");
+    console.log(score);
+//    console.log("Indices before sort:");
+//    console.log(indices);
+    indices.sort( (a,b) => (score[b] - score[a])|| (a-b)); //HOLY SHIT THE SORT ALGORITHM IS SUPER WEIRD AND IT WILL CHOOSE A STRANGE ORDERING IF THE COMPARISON GIVES 0
+//    console.log("Indices after sort:");
+//    console.log(indices);
+    var B = [];
+    for (contestant = 0; contestant < options.survivors; contestant++){
+        B.push(A[indices[contestant]]);
+        console.log((contestant + 1)+ordinalSuffix(contestant + 1) + " best score: "+(score[indices[contestant]] / nGames / playerNumber / 4).toFixed(2) + ". Number: "+(indices[contestant]+1));
+    }for (contestant = 0; contestant < options.survivors; contestant++){
+        console.log("Score of previous #"+(contestant + 1)+": "+(score[contestant] / nGames / playerNumber / 4).toFixed(2));
+    }
+    
+    B.push(A[Math.floor(Math.random() * playerNumber)]); //Add one more randomly
+    for (contestant = options.survivors + 1; contestant < playerNumber; contestant++){
+        var oneParent = Math.floor(Math.random() * (options.survivors + 1));
+        var otherParent = Math.floor(Math.random() * options.survivors);
+        if (contestant > playerNumber * options.percentMutated && oneParent!== otherParent){
+            B.push(makeAChild([B[oneParent], B[otherParent]], 0));
+        } else {
+            B.push(makeAChild([B[oneParent], B[otherParent]], learningRate));
+        }
+    }
+    return B;
+}
+    
+function ordinalSuffix(n){
+    var suffix = "th";
+    switch (n) {
+        case 1: suffix = "st";
+            break;
+        case 2: suffix = "nd";
+            break;
+        case 3: suffix = "rd";
+            break;
+    }
+    return suffix;
+}
+
+
+function makeAChild(n, mutationRate){//n is an array of networks
+    var child = n[0].clone();
+    var nParents = n.length;
+    for(var layerCount in child.layers){
+        var currentNeuronList = child.layers[layerCount].neuronList;
+        for(var neuronCount in currentNeuronList){
+            var currentInputList = currentNeuronList[neuronCount].inputs;
+            for(var inputCount in currentInputList){
+                var whichParent = Math.floor(Math.random() * nParents);
+                currentInputList[inputCount] = n[whichParent].layers[layerCount].neuronList[neuronCount].inputs[inputCount] + (Math.random() * 2 - 1)* mutationRate;
+            }
+        }
+    }
+    return child;
+}
+
 //var x = [];
 //var zeroVector = [];
 //
@@ -373,39 +505,60 @@ function improveSpecies(A, nGames, learningRate, options){
 //            }
 //        }
 //    }
-function makeAGoodBrain(nPlayers, nGames, nIterations, learningRate, options){//options = {consoleInterval:interations, writeInterval:In minutes, seed:another array}
+function makeAGoodBrain(nPlayers, nGames, nIterations, learningRate, options){//options = {consoleInterval:interations, writeInterval:In minutes, seed:another array, survivors:number of survivors each generation, mutatedPercent:how many children get mutations, genetic: true or false, return:how many brains to return}
     var A = [];
     var newBrains = nPlayers;
-    if (options.seed !== undefined){
+    var date;
+    if (options.seed !== null){
         A = options.seed;
         newBrains -= options.seed.length;
+        for (let i = 0; i < newBrains; i++){
+            A.push(makeAChild(options.seed, learningRate));
+        }
+    } else {
+        for (let i = 0; i < newBrains; i++){
+            A.push(new network([14,4,6,12,4], "random"));
+        }
     }
-    for (var i = 0; i < newBrains; i++){
-        A.push(new network([7,9,4],"random"));
-    }
+    
+    console.log("Number of contestants:");
+    console.log(A.length);
     var then = Date.now();
-    for (i = 0; i < nIterations; i++){
-        A = improveSpecies(A, nGames, learningRate, options);
+    for (let i = 0; i < nIterations; i++){
+        fs.writeFileSync('log.txt', 'Tournament number '+(i+1)+'.\n', writingCallback);
+        options.genetic = options.genetic || false;
+        if (!options.genetic){
+            A = improveSpecies(A, nGames, learningRate, options);
+        } else {
+            A = improveSpeciesGenetic(A, nGames, learningRate, options);
+        }
         var nowDate = Date.now();
         if (options.consoleInterval !== undefined){
                 if (i % options.consoleInterval == options.consoleInterval - 1){
-                console.log(i+1+' tournaments so far!');
+                    date = new Date();
+                    date.setUTCHours(date.getUTCHours() - 5);
+                    date = date.toISOString().
+                      replace(/T/, ' ').      // replace T with a space
+                      replace(/\..+/, '');
+                    console.log(i+1+' tournaments so far!');
+                    console.log("The time is "+date);
             }
         }
         if (options.writeInterval !== undefined){
             if (nowDate > then + options.writeInterval * 60000){
                 then = Date.now();
-                var date = new Date();
+                date = new Date();
                 date.setUTCHours(date.getUTCHours() - 5);
                 date = date.toISOString().
                   replace(/T/, ' ').      // replace T with a space
                   replace(/\..+/, '');
-                fs.writeFileSync("./Brains/Brain"+date+".JSON", JSON.stringify(A[0]), writingCallback);
+                fs.writeFileSync("./Brains/Brain"+date+".JSON", JSON.stringify(A.slice(0, options.return)), writingCallback);
 
             }
         }
     }
-    return A[0];
+    options.return = options.return || 1;
+    return A.splice(0, options.return);
 }
 
 
@@ -421,6 +574,7 @@ function playGameOnePlayer(brainArray, angle){
     for (var countBrain = 0; countBrain <  nBrains; countBrain++){
         fs.writeFileSync("./Brains/LastBrain.JSON", JSON.stringify(brainArray[countBrain]), writingCallback);
         var u = new universeInfo();
+        //u.dt = 0.7; //Fast speed!!!
         u.explosionLength = 0;
         var status = new gameStatus();
         u.Player = new playerTypes(u);
@@ -430,10 +584,12 @@ function playGameOnePlayer(brainArray, angle){
         status.winner = "none";
         status.onePlayer = true;
         status.stepCount = 0;
-        while(status.playing){
+        var pos = [];
+        while(status.playing && status.stepCount < 20000){
             gameStep(u,status);
-            status.stepCount += 1;
+            status.stepCount++;
         }
+        //fs.writeFileSync("./Brains/Positions.JSON", JSON.stringify(pos), writingCallback);
         scores.push(status.stepCount);
     }
     return scores;
@@ -444,11 +600,13 @@ function playTournamentOnePlayer(A, gameNumber, options){//A is an array of netw
     var scores = Array(playerNumber).fill(0);
     var angle = 0;
     for (var nthGame2 = 0; nthGame2 < gameNumber; nthGame2++){
+        angle = Math.PI * 2 * (nthGame2 + Math.random())/ gameNumber;
         var gameResult = playGameOnePlayer(A, angle);
         for (var player = 0 ; player < playerNumber; player++){
             scores[player] += gameResult[player];
         }
-        angle += Math.PI * 2/gameNumber;
+        fs.appendFileSync('log.txt', 'Game number '+(nthGame2+1)+' concluded.\n', writingCallback);
+        //console.log('Game number '+nthGame2+' concluded.');
     }
     return scores;
 }
@@ -485,27 +643,44 @@ function writingCallback(err){
 
 const fs = require('fs');
 
-var startingBrain = importBrain("./Brains/FinalBrain10-3.JSON");
-var A = makeAGoodBrain(100, 20, 100, 0.1, {consoleInterval:10, writeInterval:5, random:true, onePlayer:true, seed:[startingBrain]});
-//console.log("First tournament done.");
-//A = makeAGoodBrain(100, 5, 10, 0.1, {consoleInterval:50, writeInterval:10, random:true, onePlayer:true, seed:[A]});
-//console.log("Second tournament done.");
-//A = makeAGoodBrain(100, 5, 10, 0.1, {consoleInterval:50, writeInterval:10, random:true, onePlayer:true, seed:[A]});
-//A = makeAGoodBrain(10, 50, 10, 0.1, {consoleInterval:50, writeInterval:10, random:true, onePlayer:true, seed:[A]});
-//console.log("First tournament done. Now for the pairs");
-//A = makeAGoodBrain(5, 20, 1000, 0.1, {consoleInterval:50, writeInterval:10, random:true, onePlayer:true, seed:[A, A]});//function makeAGoodBrain(nPlayers, nGames, nIterations, learningRate, options){//options = {consoleInterval:interations, writeInterval:In minutes}
+function trainingSequence(){
+
+    var B = importBrainArray("./Brains/StartingBrain.JSON");
+    //console.log(B);
+    //console.log(typeof B);
+    //console.log(typeof B[0]);
+    //var B = [startingBrain];
+    //
+
+    var A = makeAGoodBrain(30, 2, 100, 0.01, {consoleInterval:1,
+                                                writeInterval:10,
+                                                random:false,
+                                                onePlayer:false,
+                                                genetic:true,
+                                                survivors: 5,
+                                                mutatedPercent:0.5,
+                                                seed:null,
+                                                return:5});
+    
+//(nPlayers, nGames, nIterations, learningRate, options)options = {consoleInterval:interations, writeInterval:In minutes, seed:another array, survivors:number of survivors each generation, mutatedPercent:how many children get mutations, genetic: true or false, return:how many brains to return}
 
 
+//Y calcular correlaciones de todos los pesos con la puntuación, y luego modificar la red acordemente?
+    var date = new Date();
+    date.setUTCHours(date.getUTCHours() - 5);
+    date = date.toISOString().
+      replace(/T/, ' ').      // replace T with a space
+      replace(/\..+/, '');
+    fs.writeFile("./Brains/FinalBrain"+date+".JSON", JSON.stringify(A), (err) => {
+      if (err) console.log(err);
+      console.log("Successfully Written to File.");
+    });
+}
 
-var date = new Date();
-date.setUTCHours(date.getUTCHours() - 5);
-date = date.toISOString().
-  replace(/T/, ' ').      // replace T with a space
-  replace(/\..+/, '');
-fs.writeFile("./Brains/FinalBrain"+date+".JSON", JSON.stringify(A), (err) => {
-  if (err) console.log(err);
-  console.log("Successfully Written to File.");
-});
+
+trainingSequence();
+
+
 
 
 function importBrain(path){
@@ -515,17 +690,27 @@ function importBrain(path){
     //B.__proto__ = network.prototype;
     return B;
 }
-//console.log(B.constructor.toString());
-////B.constructor.call(B);
-//console.log(B.activate([1,2,3,4,5,.6,.7]));
-//console.log(playGame(B,B));
-//console.log(B);
-//console.log(JSON.parse(B));
+
+function importBrainArray(path){
+    let rawdata = fs.readFileSync(path);
+    let B = JSON.parse(rawdata);
+    for (var i = 0; i < B.length; i++){
+        Object.setPrototypeOf(B[i], network.prototype);
+    }
+    //B.__proto__ = network.prototype;
+    return B;
+}
 
 
 
-
-
+//TESTING
+//var A = new network([14,4,6,12,4], "random");
+//var u = new universeInfo();
+//u.Player = new playerTypes(u);
+//var input = u.Player.One.observeBattle(u);
+//console.log(input);
+//var answers = A.activate(input);
+//console.log(answers);
 
 
 
